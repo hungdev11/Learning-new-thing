fcc8e66989126d4ed25ddf782430c90fd716a3a3
Hunt Willer <hungphangialai11@gmail.com>
2025-11-16 19:13:20 +0700

    Configure for GitHub Pages deployment

--- learn-frontend/vite.config.js
+++ learn-frontend/vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Learning-new-thing/',
   server: {
     proxy: {
-      '/api': 'http://localhost:8081'
-    }
-  }
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
+    },
+  },
 })
